0741d407dac9fd43f33e7e94049c3f2cb98cc195
ARROUS Thomas <thomas_arrous@yahoo.fr>
2024-08-02 18:59:02 +0200

    feat: Ajouter la classe ColorTools pour les opérations utilitaires sur les couleurs

---
+++ src/utils/ColorTools.java
@@ -0,0 +1,37 @@
+package utils;
+
+import java.awt.Color;
+import java.util.Random;
+
+/**
+ * Classe utilitaire pour les opérations sur les couleurs.
+ */
+public class ColorTools {
+    private static final Random rand = new Random();
+
+    /**
+     * Génère une couleur aléatoire.
+     * 
+     * @return une instance de {@link Color} représentant une couleur aléatoire.
+     */
+    public static Color generateRandomColor() {
+        int red = rand.nextInt(256);
+        int green = rand.nextInt(256);
+        int blue = rand.nextInt(256);
+        return new Color(red, green, blue);
+    }
+
+    /**
+     * Renvoie une couleur plus foncée basée sur la couleur donnée.
+     * 
+     * @param color la couleur de base.
+     * @return une nouvelle instance de {@link Color} qui est plus foncée que la
+     *         couleur de base.
+     */
+    public static Color getDarkerColor(Color color) {
+        int red = (int) (color.getRed() * 0.8);
+        int green = (int) (color.getGreen() * 0.8);
+        int blue = (int) (color.getBlue() * 0.8);
+        return new Color(red, green, blue);
+    }
+}
